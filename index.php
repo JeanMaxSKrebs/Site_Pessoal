eb3d38b6009de0aa76ab5773219d0bed38b91177
Unknown <jeanmaxskrebs@gmail.com>
2019-09-13 11:10:10 -0300

    Implementa a base do projeto
    Criei os arquivos index, css além de organizar as grids principais

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Site Pessoal</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div id="grid">
+        <header id="header">
+            <div class="foto">
+                foto_header
+            </div>
+            <nav id="nav">
+                nav_header
+            </nav>
+        </header>
+        <article id="article">
+            <section id="section">
+                section 1 <br>
+            </section>
+            <section id="section">
+                section 2 <br>
+            </section>
+        </article>
+        <footer id="footer">
+            my footer
+        </footer>
+    </div>
+</body>
+
+</html>
